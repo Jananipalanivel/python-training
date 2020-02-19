622398138ac2822a7ad961b9332203df4c371480
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-19 18:18:11 +0530

    Update Palindrome + emordnilaP.py

--- Palindrome + emordnilaP.py
+++ Palindrome + emordnilaP.py
@@ -3,3 +3,19 @@ while a!=a[::-1]:
   a=int(a)+int(a[::-1])
   a=str(a)
 print(a)
+
+
+#question
+Given a number N, reverse and add the number till the number is a palindrome. If the number itself is a palindrome print the number itself as the output.
+Input Format
+One integer Value N
+Constraints
+1<=N<=10^9
+Output Format
+The palindrome number got by reversing and adding the number
+Sample Input 0
+123
+Sample Output 0
+444
+Explanation 0
+123 + 321 = 444
