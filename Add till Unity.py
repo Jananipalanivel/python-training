cf529464b95b81da7ac03f2f5d7eac0bf2a8a087
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-20 11:35:41 +0530

    Create Add till Unity.py

---
+++ Add till Unity.py
@@ -0,0 +1,13 @@
+#question
+Given a number N, keep finding the digital sum of the number until it becomes a single digit number.
+Input Format
+One single Integer N
+Constraints
+1<=N<=10^18
+Output Format
+One single digit number
+Sample Input 0
+4129984
+Sample Output 0
+1
+
