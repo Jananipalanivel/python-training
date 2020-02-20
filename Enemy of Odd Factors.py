a3e6f689b8a2832f9ace3cb1ed348a0b106820e2
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-20 10:07:31 +0530

    Update Enemy of Odd Factors.py

--- Enemy of Odd Factors.py
+++ Enemy of Odd Factors.py
@@ -7,10 +7,18 @@ Constraints
 Output Format
 Enemy of Odd Factor / Friend of Odd Factor
 
-#answer
+#answer1
 from math import sqrt as s
 n=int(input())
-if (s(n)%1==0):
-    print("Enemy of Odd Factor")
+flag=0
+if (s(n)%1!=0):
+    for i in range(2,n):
+        if(n%1==0 and s(i)%1==0):
+            flag=1
+            break
 else:
+    flag=1
+if flag==1:
     print("Friend of Odd Factor")
+else:
+    print("Enemy of Odd Factor")
