10e37d64d14fd64c1d9452ba6508e860048ccbda
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-20 18:04:46 +0530

    Create Reverse_OS_Series.py

---
+++ Reverse_OS_Series.py
@@ -0,0 +1,9 @@
+#question
+1 4 5 16 17 20 21 68 69 80... a infinite series. Given a number N check whether the number will be part of the series or not. If yes print the corresponding index of the number else print NOT PRESENT.
+Input Format
+A single integer N
+Constraints
+1<=N<=2^64
+Output Format
+If present in series print only the Index
+else print "NOT PRESENT"
