548c79c57617a7213acf9e0adc398dad6f12e47e
Janani <52209332+Jananipalanivel@users.noreply.github.com>
2020-02-22 15:54:05 +0530

    Update 1,9,11,19,...... series.py

--- 1,9,11,19,...... series.py
+++ 1,9,11,19,...... series.py
@@ -1,3 +1,4 @@
+#answer
 le,i=1,0
 l=[]
 for _ in range(100):
@@ -11,3 +12,22 @@ for _ in range(100):
     if (i==2**le):
         le+=1
         i=0
+#answer 2
+n=int(input())
+fill=1
+k=0
+p=2
+c=1
+while(n):
+    s=bin(k)[2:]
+    s=s.zfill(fill)
+    s=s.replace("1","9")
+    s=s.replace("0","1")
+    print(s)
+    k+=1
+    c+=1
+    if (c>p):
+        k=0
+        p*=2
+        fill+=1
+        c=1
